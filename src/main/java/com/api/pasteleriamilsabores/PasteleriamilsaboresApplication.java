32aa4865b02b7e2f1a70607355f6ec033a918fc9
fabbbbyyyy <fabi.lillo@duocuc.cl>
2025-10-28 15:40:16 -0300

    inicio api pasteleria mil sabores

---
+++ src/main/java/com/api/pasteleriamilsabores/PasteleriamilsaboresApplication.java
@@ -0,0 +1,13 @@
+package com.api.pasteleriamilsabores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteleriamilsaboresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasteleriamilsaboresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/pasteleriamilsabores/PasteleriamilsaboresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.pasteleriamilsabores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteleriamilsaboresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
